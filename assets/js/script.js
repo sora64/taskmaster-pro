b94f7d30d83ade966a70be3e3ef35896ff4d97e8
Alan Phelps <phelpsa64@gmail.com>
2021-12-29 17:29:45 -0500

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,14 +53,10 @@ var auditTask = function(taskEL) {
   .find('span')
   .text()
   .trim();
-
-  console.log(date);
   
   // convert to moment object at 5:00pm
   var time = moment(date, 'L')
   .set('hour', 17);
-
-  console.log(time);
   
   // remove any old classes from element
   $(taskEL).removeClass('list-group-item-warning list-group-item-danger');
@@ -86,7 +82,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -223,12 +219,20 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
+    $(this).addClass('dropover');
+    $('.bottom-trash').addClass('bottom-trash-drag');
   },
   deactivate: function(event) {
+    $(this).removeClass('dropover');
+    $('.bottom-trash').removeClass('bottom-trash-drag');
   },
   over: function(event) {
+    $(event.target).addClass('dropover-active');
+    $('.bottom-trash').addClass('bottom-trash-active');
   },
   out: function(event) {
+    $(event.target).removeClass('dropover-active');
+    $('.bottom-trash').removeClass('bottom-trash-active');
   },
   update: function(event) {
     // array to store the task data in
@@ -295,3 +299,9 @@ $("#modalDueDate").datepicker({
 
 // load tasks for the first time
 loadTasks();
+
+setInterval(function() {
+  $('.card .list-group-item').each(function(index, el) {
+    auditTask(el);
+  });
+}, 1800000);
